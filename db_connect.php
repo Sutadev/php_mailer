4481b1365440f446c40d58fa7805e610a8a7f920
Sutadev <162961332+Sutadev@users.noreply.github.com>
2024-11-01 00:21:49 +0530

    Add files via upload

---
+++ db_connect.php
@@ -0,0 +1,11 @@
+<?php
+
+//conect database
+$servername = "localhost";
+$username = "root";
+$password ="";
+$dbname = "suta_test";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
